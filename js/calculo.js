68575c453ea58996dcebd01a28ac8c78f0f43a50
jarad hildebrando <jarad_hildebrando@hotmail.com>
2020-11-17 18:37:16 -0300

    concluido

--- js/calculo.js
+++ js/calculo.js
@@ -3,6 +3,8 @@
 var PN = 0
 var E = 0
 var VN = 0
+var i = 1
+var api = ''
 
 
 
@@ -25,8 +27,24 @@ if (VN <= 12 ) {
     document.getElementById('texto').value = ''
     document.getElementById('item').value = ''
 }
- post.innerHTML = (VITEM + ' precisa ser colocado no cardápio por '+ PN ) 
+ post.innerHTML = (VITEM + ' precisa ser colocado no cardápio por R$'+ PN + '   ' ) 
+ api += (i + '° ' + post.innerHTML)
+ i++
     document.body.appendChild(post)
+
+    
+}
+
+var local = document.getElementById('cardapio')
+
+function enviarIffod() {
+     window.location.href ='https://api.whatsapp.com/send?phone=5581988977526&text=' +api +''
+    console.log(api)
+    var postando = document.createElement('a')
+    postando.href ='https://api.whatsapp.com/send?phone=5581988977526&text=Oilojastropical'
+    local = document.getElementById('cardapio')
+
+    local.appendChild(postando)
 }
 
 
